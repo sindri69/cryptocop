e319a69e729fab9470ff82edb06afb718ec75d70
sindri69 <sindrii17@ru.is>
2020-11-08 22:58:45 +0000

    added jsonproperty to exchangedto

--- Cryptocop.Software.API/Cryptocop.Software.API.Models/Dtos/ExhangeDto.cs
+++ Cryptocop.Software.API/Cryptocop.Software.API.Models/Dtos/ExhangeDto.cs
@@ -1,4 +1,5 @@
 using System;
+using Newtonsoft.Json;
 
 namespace Cryptocop.Software.API.Models.Dtos
 {
@@ -6,16 +7,17 @@ namespace Cryptocop.Software.API.Models.Dtos
   //í large assignment 1 erfa classarnir hypermediamodel
     public class ExchangeDto
     {
+        [JsonProperty("exchange_id")]
         public int Id { get; set; }
-
+        [JsonProperty("exchange_name")]
         public string Name { get; set; }
-
+        [JsonProperty("exchange_slug")]
         public string Slug { get; set; }
-
+        [JsonProperty("base_asset_symbol")]
         public string AssetSymbol { get; set; }
-
+        [JsonProperty("price_usd")]
         public float? PriceInUsd {get; set;}
-
+        [JsonProperty("last_trade_at")]
         public DateTime? LastTrade {get; set;}
 
     }
